0f871b734cf41de0f2ec6dd408c2cad596852c05
ZaydAbdullayev <zeyyadabuzayd@gmail.com>
2023-07-26 16:24:57 +0500

    new section update

--- src/pages/myFavorite/favorite.jsx
+++ src/pages/myFavorite/favorite.jsx
@@ -1,125 +1,78 @@
-import React, {
-   useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./favorite.css";
-import { FiStar } from "react-icons/fi"
-import { BsStarFill } from "react-icons/bs"
-import { Link } from "react-router-dom";
+import { FiStar } from "react-icons/fi";
+import { BsStarFill } from "react-icons/bs";
+import { useParams } from "react-router-dom";
+import { ApiGetService } from "../../services/api.service";
 
 export const MyFavorite = () => {
+  const user = JSON.parse(localStorage.getItem("customer")) || [];
+
   const [favorite, setFavorite] = useState(false);
-
-
-
+  const id = useParams().id;
+  const user_id = user?.id;
+  const [shop, setShop] = useState([]);
+  console.log(shop);
+  useEffect(() => {
+    ApiGetService.fetching(`get/favRes/${user_id}`)
+      .then((res) => {
+        setShop(res?.data?.innerData);
+      })
+      .catch((err) => console.log(err));
+    window.scrollTo(0, 0);
+  }, [user_id]);
   return (
     <div className="my_favorite">
-     <div>
+      <div>
         <div className="my_account">
           <div>
-          <h1>My Favorite</h1>
-          <p>Dashboard</p>
+            <h1>My Favorite</h1>
+            <p>Dashboard</p>
           </div>
           <hr />
           <p>Restaurant</p>
         </div>
-         <div className="fovorite_card" >
-          <div> 
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQia7DpZRe3-d7D3tzHidOTVsa_LClnmK1uc55heXzWvkBWEb_1dG6HdMXe8MytNNp3MEU&usqp=CAU" alt="" />
-        <p>JIMMY'S BURGERS</p> 
-        <span onClick={()=>setFavorite(1)}>
-        {favorite >= 1 ? <BsStarFill /> : <FiStar />}
-        </span>
-        <span onClick={()=>setFavorite(2)}>
-        {favorite >= 2 ? <BsStarFill /> : <FiStar />}
-        </span>
-        <span onClick={()=>setFavorite(3)}>
-        {favorite >= 3 ? <BsStarFill /> : <FiStar />}
-        </span>
-        <span onClick={()=>setFavorite(4)}>
-        {favorite >= 4 ? <BsStarFill /> : <FiStar />}
-        </span>
-        <span onClick={()=>setFavorite(5)}>
-        {favorite >= 5 ? <BsStarFill /> : <FiStar />}
-        </span>
-        <p>===========</p>
-        <button>Order</button>
-          </div>
-          <div > 
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQia7DpZRe3-d7D3tzHidOTVsa_LClnmK1uc55heXzWvkBWEb_1dG6HdMXe8MytNNp3MEU&usqp=CAU" alt="" />
-        <p>JIMMY'S BURGERS</p> 
-        <span onClick={()=>setFavorite(1)}>
-        {favorite >= 1 ? <BsStarFill /> : <FiStar />}
-        </span>
-        <span onClick={()=>setFavorite(2)}>
-        {favorite >= 2 ? <BsStarFill /> : <FiStar />}
-        </span><span onClick={()=>setFavorite(3)}>
-        {favorite >= 3 ? <BsStarFill /> : <FiStar />}
-        </span><span onClick={()=>setFavorite(4)}>
-        {favorite >= 4 ? <BsStarFill /> : <FiStar />}
-        </span><span onClick={()=>setFavorite(5)}>
-        {favorite >= 5 ? <BsStarFill /> : <FiStar />}
-        </span>
-        <p>===========</p>
-        <button>Order</button>
-          </div>
-          <div> 
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQia7DpZRe3-d7D3tzHidOTVsa_LClnmK1uc55heXzWvkBWEb_1dG6HdMXe8MytNNp3MEU&usqp=CAU" alt="" />
-        <p>JIMMY'S BURGERS</p> 
-        <span onClick={()=>setFavorite(1)}>
-        {favorite >= 1 ? <BsStarFill /> : <FiStar />}
-        </span>
-        <span onClick={()=>setFavorite(2)}>
-        {favorite >= 2 ? <BsStarFill /> : <FiStar />}
-        </span><span onClick={()=>setFavorite(3)}>
-        {favorite >= 3 ? <BsStarFill /> : <FiStar />}
-        </span><span onClick={()=>setFavorite(4)}>
-        {favorite >= 4 ? <BsStarFill /> : <FiStar />}
-        </span><span onClick={()=>setFavorite(5)}>
-        {favorite >= 5 ? <BsStarFill /> : <FiStar />}
-        </span>
-        <p>===========</p>
-        <button>Order</button>
-          </div>
-          <div> 
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQia7DpZRe3-d7D3tzHidOTVsa_LClnmK1uc55heXzWvkBWEb_1dG6HdMXe8MytNNp3MEU&usqp=CAU" alt="" />
-        <p>JIMMY'S BURGERS</p> 
-        <span onClick={()=>setFavorite(1)}>
-        {favorite >= 1 ? <BsStarFill /> : <FiStar />}
-        </span>
-        <span onClick={()=>setFavorite(2)}>
-        {favorite >= 2 ? <BsStarFill /> : <FiStar />}
-        </span><span onClick={()=>setFavorite(3)}>
-        {favorite >= 3 ? <BsStarFill /> : <FiStar />}
-        </span><span onClick={()=>setFavorite(4)}>
-        {favorite >= 4 ? <BsStarFill /> : <FiStar />}
-        </span><span onClick={()=>setFavorite(5)}>
-        {favorite >= 5 ? <BsStarFill /> : <FiStar />}
-        </span>
-        <p>===========</p>
-        <button>Order</button>
-          </div>
-          <div> 
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQia7DpZRe3-d7D3tzHidOTVsa_LClnmK1uc55heXzWvkBWEb_1dG6HdMXe8MytNNp3MEU&usqp=CAU" alt="" />
-        <p>JIMMY'S BURGERS</p> 
-        <span onClick={()=>setFavorite(1)}>
-        {favorite >= 1 ? <BsStarFill /> : <FiStar />}
-        </span>
-        <span onClick={()=>setFavorite(2)}>
-        {favorite >= 2 ? <BsStarFill /> : <FiStar />}
-        </span><span onClick={()=>setFavorite(3)}>
-        {favorite >= 3 ? <BsStarFill /> : <FiStar />}
-        </span><span onClick={()=>setFavorite(4)}>
-        {favorite >= 4 ? <BsStarFill /> : <FiStar />}
-        </span><span onClick={()=>setFavorite(5)}>
-        {favorite >= 5 ? <BsStarFill /> : <FiStar />}
-        </span>
-        <p>===========</p>
-        <button>
-        <Link to="/catalog">order</Link>
-        </button>
-          </div>
+        <div className="fovorite_card">
+          {shop?.map((item, index) => {
+            return (
+              <div key={item.username || index}>
+                <figure>
+                  <img src={item.img} alt="" />
+                </figure>
+                <p style={{ flex: "1" }}>{item.username}</p>
+                <span
+                  onClick={() =>
+                    setFavorite({
+                      id: id,
+                      state: true,
+                      name: shop.name,
+                      user_id: user_id,
+                      img: shop.img,
+                      rating: favorite,
+                    })
+                  }
+                >
+                  {favorite >= 1 && item.id ? <BsStarFill /> : <FiStar />}
+                </span>
+                <span onClick={() => setFavorite(2)}>
+                  {favorite >= 2 && item.id ? <BsStarFill /> : <FiStar />}
+                </span>
+                <span onClick={() => setFavorite(3)}>
+                  {favorite >= 3 && item.id ? <BsStarFill /> : <FiStar />}
+                </span>
+                <span onClick={() => setFavorite(4)}>
+                  {favorite >= 4 && item.id ? <BsStarFill /> : <FiStar />}
+                </span>
+                <span onClick={() => setFavorite(5)}>
+                  {favorite >= 5 && item.id ? <BsStarFill /> : <FiStar />}
+                </span>
+                <p>===========</p>
+                <button>Order</button>
+              </div>
+            );
+          })}
         </div>
       </div>
-      
     </div>
-  )
-}
+  );
+};
--- src/pages/myProfil/profil.jsx
+++ src/pages/myProfil/profil.jsx
@@ -1,57 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./profil.css";
-
+import { ApiGetService, ApiUpdateService } from "../../services/api.service";
+import { PatternFormat, NumericFormat } from "react-number-format";
 
 export const MyProfil = () => {
+  const id = JSON.parse(localStorage.getItem("customer")).id || null;
+  const [user, setUser] = useState([]);
+
+  const phone = user?.phone?.split("+")[1];
+  console.log(phone);
+
+  useEffect(() => {
+    ApiGetService.fetching(`get/user/${id}`)
+      .then((res) => {
+        setUser(res?.data?.innerData);
+      })
+      .catch((err) => console.log(err));
+  }, [id]);
+
+  useEffect(() => {
+    ApiUpdateService.fetching(`update/user/${id}`)
+      .then((res) => {
+        setUser(res);
+      })
+      .catch((err) => console.log(err));
+  }, [id]);
+
   return (
     <div className="my_profil">
-     <div>
-     <div>
-        <div className="my_account">
-          <div>
-          <h1>My Account</h1>
-          <p>Account Orders</p>
+      <div>
+        <div>
+          <div className="my_account">
+            <div>
+              <h1>My Account</h1>
+              <p>Account Orders</p>
+            </div>
+            <hr />
           </div>
-          <hr />
-        </div>
-         <div className="profil_card">
-          <div>
-          <p>Start Your Order</p>
-   <form>
-            <section>
-            <span>Username</span>
-            <input placeholder="Name..." type="text" />
-            </section>
-            <section>
-            <span>Fullname</span>
-            <input
-             placeholder="Text"
-              type="text" />
-            </section>
-            <section>
-            <span>phone</span>
-            <input
-            placeholder="+998 99 777_77_77 "
-            required
-            type="number"
-            name="phone"
-           />
-            </section>
-            <section>
-            <span>Count</span>
-            <input
-             placeholder="777"
-             type="number" />
-            </section>
-   </form>
-          <button>Find Food</button>
+          <div className="profil_card">
+            <div>
+              <p>Start Your Order</p>
+              <form>
+                <section>
+                  <span>Username</span>
+                  <input
+                    placeholder="Name..."
+                    type="text"
+                    defaultValue={user?.username}
+                  />
+                </section>
+                <section>
+                  <span>Password</span>
+                  <input
+                    placeholder="******"
+                    type="password"
+                    defaultValue={user?.password}
+                  />
+                </section>
+                <section>
+                  <span>phone</span>
+                  <NumericFormat
+                    value={phone}
+                    name="phone"
+                    prefix="+"
+                    allowLeadingZeros
+                    thousandSeparator=" "
+                  />
+                </section>
+                <section>
+                  <span>food</span>
+                  <input placeholder="777" type="number" />
+                </section>
+              </form>
+              <button>Find Food</button>
+            </div>
           </div>
-       
         </div>
-      
-
       </div>
-     </div>
     </div>
-  )
-}
+  );
+};
